cfa1e6c1e36da542b4967e0bfcb0312d208553c0
Paul Hidalgo <paulesh5@gmail.com>
2023-07-23 23:08:14 -0500

    Login

---
+++ src/LoginUsuario.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class LoginUsuario {
+    private JLabel Nombre;
+    private JLabel Contra;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+    private JButton inicioBoton;
+    private JButton registrarUsuarioButton;
+}
